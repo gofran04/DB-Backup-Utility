1a5f6e23e435f38c9550e732db2dba0503390268
gofran04 <docreader04@gmail.com>
2025-06-10 19:41:28 +0300

    setup Back-job model and migration

---
+++ app/Http/Controllers/BackupJobController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBackupJobRequest;
+use App\Http\Requests\UpdateBackupJobRequest;
+use App\Models\BackupJob;
+
+class BackupJobController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBackupJobRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BackupJob $backupJob)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BackupJob $backupJob)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBackupJobRequest $request, BackupJob $backupJob)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BackupJob $backupJob)
+    {
+        //
+    }
+}
