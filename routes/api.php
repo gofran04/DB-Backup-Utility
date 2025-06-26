7bdfadc988f360d3fa76877dd147a3818de0bc72
gofran04 <docreader04@gmail.com>
2025-06-26 20:04:50 +0300

    Refactor:backup operation by test db connection inside the DatabaseBackupService, and move testConnection method to TestDatabaseConnectionService instead of being in DatabaseConnectionControllers

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DatabaseConnectionController;
 use App\Http\Controllers\BackupJobController;
+use App\Services\TestDatabaseConnectionService;
 
-Route::get('database-connections/test/{db_id}',[DatabaseConnectionController::class, 'testConnection']);
 Route::apiResource('database-connections',DatabaseConnectionController::class);
 Route::apiResource('backup-jobs',BackupJobController::class)->except(['update']);
-
-
-
+Route::get('test-db-connection/{id}',[TestDatabaseConnectionService::class,'testConnection']);
