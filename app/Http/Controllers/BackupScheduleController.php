dccc965e42a5d85f239e64b9e3f2df5b92e8cbd3
gofran04 <docreader04@gmail.com>
2025-07-16 17:57:53 +0300

    refactor index() by usig APIResponeTrait to return the respose. build show() to view specific task schedule

--- app/Http/Controllers/BackupScheduleController.php
+++ app/Http/Controllers/BackupScheduleController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\BackupSchedule;
 use Illuminate\Http\Request;
 use App\Http\Resources\BackupScheduleResource;
+use Symfony\Component\HttpFoundation\Response;
+use App\Traits\ApiResponseTrait;
 
 class BackupScheduleController extends Controller
 {
+    use ApiResponseTrait;
 
     public function index()
     {
-        return BackupScheduleResource::collection(BackupSchedule::all());
+        return $this->successResponse(
+            BackupScheduleResource::collection(BackupSchedule::all()),
+            'Task schedules retrieved',Response::HTTP_OK);
     }
 
     public function store(Request $request)
@@ -21,7 +26,9 @@ class BackupScheduleController extends Controller
 
     public function show(BackupSchedule $backupSchedule)
     {
-        //
+        return $this->successResponse(
+            new BackupScheduleResource($backupSchedule),
+            'Task schedule retrieved.',Response::HTTP_OK);
     }
 
 
