3b58a60e637559d2697da3af8af70c0c0c1ebcac
gofran04 <docreader04@gmail.com>
2025-07-19 18:35:00 +0300

    refactor: use getJson() insted of get()

--- tests/Feature/DBConnectionsTest.php
+++ tests/Feature/DBConnectionsTest.php
@@ -15,7 +15,7 @@ class DBConnectionsTest extends TestCase
     public function test_return_all_db_connections()
     {
         DatabaseConnection::factory()->count(10)->create();
-        $response = $this->get('api/database-connections');
+        $response = $this->getJson('api/database-connections');
 
         $response->assertOk();
         $this->assertDatabaseCount('database_connections', 10);
@@ -24,7 +24,7 @@ class DBConnectionsTest extends TestCase
     public function test_return_specific_db_connection()
     {
         $db_connection = DatabaseConnection::factory()->create();
-        $response = $this->get('api/database-connections/'.$db_connection->id);
+        $response = $this->getJson('api/database-connections/'.$db_connection->id);
 
         $response->assertOk();
         $this->assertDatabaseCount('database_connections', 1);
