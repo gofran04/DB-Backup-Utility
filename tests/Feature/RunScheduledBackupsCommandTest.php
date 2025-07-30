a8f6a87b9e459fb116f8ed6be031ab89b2bc7176
gofran04 <docreader04@gmail.com>
2025-07-30 19:05:34 +0300

    test throwing exception and catch it when running backup:scedule command

--- tests/Feature/RunScheduledBackupsCommandTest.php
+++ tests/Feature/RunScheduledBackupsCommandTest.php
@@ -8,6 +8,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\File;
 use Carbon\Carbon;
+use App\Exceptions\BackupFailedException;
+use App\Services\DatabaseBackupService;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ScheduledBackupFailed;
+use Mockery;
 
 class RunScheduledBackupsCommandTest extends TestCase
 {
@@ -24,6 +29,7 @@ class RunScheduledBackupsCommandTest extends TestCase
 
     protected function tearDown(): void
     {
+        Mockery::close();
         File::deleteDirectory(storage_path('app/test-backups'));
         parent::tearDown();
     }
@@ -61,5 +67,37 @@ class RunScheduledBackupsCommandTest extends TestCase
             ->assertExitCode(0);
         
         $this->assertDatabaseCount('backup_jobs', 0); // No jobs created
+    }
+
+    public function test_throws_BackupFailedException_and_catches_it()
+    {
+        // STEP 1: Start with faking notifications. should be in the beginning
+        Notification::fake();
+
+        $task = BackupSchedule::factory()->create([
+            'frequency'       => 'every_minute',
+            'cron_expression' => '* * * * *'
+        ]);
+
+        $mock = Mockery::mock('overload:' . DatabaseBackupService::class);
+        $mock->shouldReceive('backupUsingDbId')
+            ->andThrow(new BackupFailedException('Backup Operation Failed.', 'failed'));
+
+
+        $this->artisan('backup:schedule')
+            ->expectsOutput("Running backup for schedule ID: {$task->id}")
+            ->expectsOutput('❌ Backup failed: Backup Operation Failed.')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseHas('backup_jobs', [
+            'database_connection_id' => $task->dbConnection->id,
+            'status'                 => 'failed',
+        ]);
+
+
+        Notification::assertSentTo(
+            new \Illuminate\Notifications\AnonymousNotifiable,
+            ScheduledBackupFailed::class
+        );
     }
 }
